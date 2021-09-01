21f765fcb69cbaa866009796453e88b860317ff1
joaquimborges <joaquim.borges1993@gmail.com>
2021-09-01 16:12:17 -0300

    ajuste exercicio conversao romanos

--- src/main/java/com/example/boocampspring/controllers/CoverRomanosController.java
+++ src/main/java/com/example/boocampspring/controllers/CoverRomanosController.java
@@ -1,5 +1,6 @@
 package com.example.boocampspring.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CoverRomanosController {
 
-    public int [] numeros = {100, 5, 10};
-    public String [] roman = {"C", "V", "X"};
+    public int [] numeros = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    public String [] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
+    @GetMapping( value = "/coverRoman/{num}")
     public String romanos(@PathVariable int num){
-
 
         StringBuilder romanos = new StringBuilder();
         for (int i = 0; i < numeros.length; i++){
